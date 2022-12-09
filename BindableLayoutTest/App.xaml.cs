bc1ab8ead519d636cb89fc520345a04f98f38b44
Martin Mareček <martin.marecek@solitea.cz>
2022-12-09 07:48:52 +0100

    Add project files.

---
+++ BindableLayoutTest/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace BindableLayoutTest;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ BindableLayoutTest/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace BindableLayoutTest;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+
+        var items = Enumerable.Range(1, 200)
+            .Select(_ => $"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's...: {_}")
+            .ToList();
+
+		BindableLayout.SetItemsSource(InnerVerticalStackLayout, items);
+    }
+}
+
